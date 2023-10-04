7c3e0d9eddd3226990a559ee492a3cd55509c4a7
Rodrigo Higuera <ro@higuera.mx>
2023-10-03 19:09:35 -0600

    use explicit json data for non 200 http response

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -46,11 +46,13 @@ final class RemoteFeedLoaderTests: XCTestCase {
 	
 	func test_load_deliversErrorOnNon200HTTPResponse() {
 		let (sut, client) = makeSUT()
-				
+
 		let samples = [199, 201, 300, 400, 500]
+		
 		samples.enumerated().forEach { index, code in
 			expect(sut, toCompleteWithResult: .failure(.invalidData)) {
-				client.complete(withStatusCode: code, at: index)
+				let json = makeItemsJSON([])
+				client.complete(withStatusCode: code, data: json, at: index)
 			}
 		}
 	}
@@ -157,7 +159,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
 			messages[index].completion(.failure(error))
 		}
 		
-		func complete(withStatusCode code: Int, data: Data = Data(),  at index: Int = 0) {
+		func complete(withStatusCode code: Int, data: Data,  at index: Int = 0) {
 			let response = HTTPURLResponse(
 				url: requestedURLs[index],
 				statusCode: code,
